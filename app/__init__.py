14a9aebf24e78215e60c9998f8bfba89a8cb8f50
jhoany londoño <jlondo97@eafit.edu.co>
2020-05-31 11:15:24 -0500

    add changes in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask import render_template
+from flask import *
 
 
 app = Flask(__name__)
@@ -12,9 +13,23 @@ def index():
     return render_template('index.html')  # Regresa un string
 
 
-@app.route('/2')  # Decorador o wrap
+@app.route('/Metodo2', methods=['GET', 'POST'])  # Decorador o wrap
 def Metodo2():
-    return 'Hello, people'  # Regresa un string
+    incremento = request.form.get('incremento')
+    funcion = request.form.get('funcion')
+    valor_inicial = request.form.get('Valor_inicial')
+    iteraciones = request.form.get('Iteraciones')
+
+    if request.method == 'POST':
+        suma = Suma(incremento, funcion)
+        print(incremento, funcion, valor_inicial, iteraciones)
+
+    return render_template('busquedasIncrementales.html', suma=suma)
 
 
-app.run(debug=False, port=8000)  # Se encarga de ejecutar el servidor
+def Suma(a, b):
+    suma = int(a) + int(b)
+    return str(suma)
+
+
+app.run(debug=True)  # Se encarga de ejecutar el servidor
