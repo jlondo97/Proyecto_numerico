1e90daf2f8e1561d38bc1f51da1104e17bcd6a77
jhoany londoño <jlondo97@eafit.edu.co>
2020-05-31 18:40:25 -0500

    arreglo a metodo de biseccion y agregando metodo de regla falsa

--- app/src/metodos/EcuacionesDeUnaVariable/MetodosPorIntervalos/__init__.py
+++ app/src/metodos/EcuacionesDeUnaVariable/MetodosPorIntervalos/__init__.py
@@ -1,2 +1,3 @@
 from metodos.EcuacionesDeUnaVariable.MetodosPorIntervalos.busquedasIncrementales import *
 from metodos.EcuacionesDeUnaVariable.MetodosPorIntervalos.metodoBiseccion import *
+from metodos.EcuacionesDeUnaVariable.MetodosPorIntervalos.reglafalsa import *
--- app/src/metodos/EcuacionesDeUnaVariable/MetodosPorIntervalos/metodoBiseccion.py
+++ app/src/metodos/EcuacionesDeUnaVariable/MetodosPorIntervalos/metodoBiseccion.py
@@ -2,6 +2,7 @@ from __future__ import division
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 
+
 class MetodoBiseccion:
     f = Function('fx')
 
@@ -14,40 +15,43 @@ class MetodoBiseccion:
         self.funcion = funcion
 
     def metodoBiseccion(self):
-        self.xi = float(self.xi) 
+        self.xi = float(self.xi)
         self.xs = float(self.xs)
         self.tolerancia = float(self.tolerancia)
         self.iteraciones = float(self.iteraciones)
-        
+
         f = parse_expr(self.funcion)
         x = Symbol('x')
-        fxi = f.subs(x,self.xi)
-        fxs = f.subs(x,self.xs)
+        fxi = f.subs(x, self.xi)
+        fxs = f.subs(x, self.xs)
         if fxi == 0:
-            print(repr(self.xi) + " es una raiz")
+            mensaje = (repr(self.xi) + " es una raiz")
+            return mensaje
 
         elif fxs == 0:
-            print(repr(self.xs) + " es una raiz")
+            mensaje = (repr(self.xs) + " es una raiz")
+            return mensaje
 
         elif fxi * fxs > 0:
-            print("El intervalo no posee una raiz")
+            mensaje = ("El intervalo no posee una raiz")
+            return mensaje
 
         else:
             xm = (self.xi + self.xs) / 2
             cont = 1
-            fxm = f.subs(x,xm)
+            fxm = f.subs(x, xm)
             error = self.tolerancia + 1
             while fxm != 0 and error > self.tolerancia and cont < self.iteraciones:
                 if fxi * fxm < 0:
                     self.xs = xm
-                    fxs = f.subs(x,self.xs)
+                    fxs = f.subs(x, self.xs)
                 else:
                     self.xi = xm
-                    fxi = f.subs(x,self.xi)
+                    fxi = f.subs(x, self.xi)
 
                 xaux = xm
                 xm = (self.xi + self.xs) / 2
-                fxm = f.subs(x,xm)
+                fxm = f.subs(x, xm)
                 error = abs(xm - xaux)
                 cont += 1
 
@@ -55,10 +59,10 @@ class MetodoBiseccion:
                 mensaje = (repr(xm) + " es una raiz")
                 return mensaje
             elif error < self.tolerancia:
-                mensaje = (repr(xm) + " se aproxima a una raiz de la función, con una tolerancia de: " + str(self.tolerancia))
+                mensaje = (repr(
+                    xm) + " se aproxima a una raiz de la función, con una tolerancia de: " + str(self.tolerancia))
                 return mensaje
             else:
                 mensaje = ("Excedio el numero de iteraciones posible")
                 return mensaje
-
 
