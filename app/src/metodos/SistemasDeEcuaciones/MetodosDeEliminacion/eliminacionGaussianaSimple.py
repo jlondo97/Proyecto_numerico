a6ec70138848f202ae70ad96ff517771a1c985b5
unknown <raavendanp@eafit.edu.co>
2020-06-01 14:39:15 -0500

    mejorado

--- app/src/metodos/SistemasDeEcuaciones/MetodosDeEliminacion/eliminacionGaussianaSimple.py
+++ app/src/metodos/SistemasDeEcuaciones/MetodosDeEliminacion/eliminacionGaussianaSimple.py
@@ -20,33 +20,22 @@ class EliminacionGaussianaSimple:
                 #print("\nM" + str(i) + str(k) + " = " + str(multiplicador))
                 for j in range(k,self.n+2):
                     self.A[i][j-1] = self.A[i][j-1] - multiplicador*self.A[k-1][j-1]
-        x = []
-        for i in range(self.n):
-            x.append([0])
-
-        for i in range(self.n,0,-1):
-            sumatoria = 0
-            for p in range(i+1, self.n+1, 1):
-                sumatoria += self.A[i-1][p-1]*x[p-1]
-            x[i-1] = (self.A[i-1][self.n] - sumatoria)/ self.A[i-1][i-1]
-       
-        return(x)
+        
+        return sustitucionRegresiva(self.A,self.n)
+        
                 
         
-        
+def sustitucionRegresiva(A,n):
+   
+    x = []
+    for i in range(n):
+        x.append([0])
 
-    # def sustitucionRegresiva(self):
-    #     self.n = int(self.n)
-    #     x = []
-    #     for i in range(self.n):
-    #         x.append([0])
-
-    #     for i in range(self.n,0,-1):
-    #         sumatoria = 0
-    #         for p in range(i+1, self.n+1, 1):
-    #             sumatoria += self.A[i-1][p-1]*x[p-1]
-    #         x[i-1] = (self.A[i-1][self.n] - sumatoria)/ self.A[i-1][i-1]
-       
-    #     return(x)
+    for i in range(n,0,-1):
+        sumatoria = 0
+        for p in range(i+1, n+1, 1):
+            sumatoria += A[i-1][p-1]*x[p-1]
+        x[i-1] = (A[i-1][n] - sumatoria)/ A[i-1][i-1]
+    return(x)
 
 
